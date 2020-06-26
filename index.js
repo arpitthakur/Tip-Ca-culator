d755e5305432e60eec4c04597c697acdf6ee6cf2
Arpit Thakur <arpitthakut722@gmail.com>
2020-06-26 11:33:02 +0530

    added half function in index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 document.getElementById('calc').addEventListener('click',calculator);
+function clalculator(e){
+    e.preventDefault()
+    let amount = document.getElementById('amount').value;
+    let discount = document.getElementById('discount').value;
+    let people = document.getElementById('people').valuw;
 
+    if (amount === "" ||discount === 0 || people === ""){
+        
+        alert("You need to fill in all the fields!");
+    }
+    
+}
